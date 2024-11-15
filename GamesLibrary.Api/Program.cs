2f02b31f7ea87abcec39e24fce7622d1aabe4597
c-kirkeby <christian@christiankirkeby.com>
2024-11-15 15:54:12 +1000

    chore: upgrade .net 8 > 9.

--- GamesLibrary.Api/Program.cs
+++ GamesLibrary.Api/Program.cs
@@ -6,14 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(options => options.RouteTemplate = "openapi/{documentName}.json");
+    app.MapOpenApi();
     app.MapScalarApiReference();
 }
 
